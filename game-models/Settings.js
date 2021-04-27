f1ba935271d5d349b58c54df8671a5241e6ab524
Keithen Shepard <keithenshepard@C02DTCN8MD6M.tld>
2021-04-27 15:49:50 -0400

    Models with board, round, and game made

---
+++ game-models/Settings.js
@@ -0,0 +1,9 @@
+//Actions
+const actions = ['FOLD', 'CHECK', 'CALL', 'RAISE']
+
+/* GAME SETTINGS */
+const NUM_ROUNDS = 100;
+const SMALL_BLIND = 25;
+const BIG_BLIND = 50;
+
+let CHIP_STACKS = {'user': 500, 'comp': 500}
